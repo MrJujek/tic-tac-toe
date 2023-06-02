ac9316b05ae34394182a3037ed87697c9af8bd93
MrJujek <julian.dworzycki@gmail.com>
2023-06-02 15:12:37 +0000

    feat: :sparkles: check vertical and horizontal for winning lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,28 +2,257 @@ import { Component, OnInit } from '@angular/core';
 import { ai } from './components/ai/ai.component';
 import { GameBoardService } from './services/gameBoard.service';
 
-// import { makeMove } from './features/makeMove.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "tic-tac-toe"
-  text1 = 'Hello World';
-  count1 = 0;
-  ai = new ai();
-  columns: number[] = Array.from({ length: 10 }, (_, i) => i + 1);
-  rows: number[] = Array.from({ length: 10 }, (_, i) => i + 1);
+  rowInput: string = "20";
+  columnInput: string = "20";
+  columns: number[] = [];
+  rows: number[] = [];
+  cellValues: string[][] = [];
+  currentPlayer: string = 'X';
+  gameOver: boolean = false;
+  winner: string = '';
+  playerScores: { [key: string]: number } = { X: 0, O: 0 };
+  winningScore: number = 5;
 
-  constructor(private gameBoardService: GameBoardService) { }
+  start() {
+    console.log("start");
 
-
-  ngOnInit() {
-    this.ai.countMove();
+    this.rows = Array.from({ length: parseInt(this.rowInput) }, (_, i) => i + 1);
+    this.columns = Array.from({ length: parseInt(this.columnInput) }, (_, i) => i + 1);
+    this.cellValues = this.initializeCellValues(parseInt(this.rowInput), parseInt(this.columnInput));
+    this.currentPlayer = 'X';
+    this.gameOver = false;
+    this.winner = '';
+    this.playerScores = { X: 0, O: 0 };
   }
 
-  click1() {
-    this.count1++;
+  initializeCellValues(rows: number, columns: number): string[][] {
+    const cellValues: string[][] = [];
+    for (let i = 0; i < rows; i++) {
+      const row: any[] = [];
+      for (let j = 0; j < columns; j++) {
+        row.push('');
+      }
+      cellValues.push(row);
+    }
+    return cellValues;
+  }
+
+  makeMove(row: number, column: number) {
+    if (!this.gameOver && this.cellValues[row][column] == '') {
+      this.cellValues[row][column] = this.currentPlayer;
+
+      console.log("makeMove");
+      this.ai()
+
+      // this.checkWinCondition(row, column);
+      if (!this.gameOver) {
+        this.switchPlayer();
+        this.makeComputerMove();
+      }
+    }
+  }
+
+  ai() {
+    // Check for a draw
+    if (this.isBoardFull()) {
+      this.setDraw();
+      return;
+    }
+
+    // console.log("ai");
+
+    let array = []
+
+    // //z lewej dol w gore prawo
+    // for (let minus = 0; minus < this.cellValues.length; minus++) {
+    //   for (let i = 0; i < this.cellValues.length; i++) {
+    //     for (let j = 0; j < this.cellValues.length; j++) {
+    //       if (i - minus == j) {
+    //         // console.log(this.cellValues[i][j])
+    //         array.push(this.cellValues[i][j])
+    //       }
+    //     }
+    //   }
+    // }
+    // console.log(array)
+
+    // array = []
+    // for (let plus = 1; plus < this.cellValues.length; plus++) {
+    //   for (let i = 0; i < this.cellValues.length; i++) {
+    //     for (let j = 0; j < this.cellValues.length; j++) {
+    //       if (i + plus == j) {
+    //         // console.log(this.cellValues[i][j])
+    //         array.push(this.cellValues[i][j])
+    //       }
+    //     }
+    //   }
+    // }
+    // console.log(array)
+
+    // array = []
+
+    // //z prawej gory w dol lewo
+    // for (let minus = 1; minus <= this.cellValues.length; minus++) {
+    //   for (let i = this.cellValues.length - 1; i >= 0; i--) {
+    //     for (let j = this.cellValues.length - 1; j >= 0; j--) {
+    //       if (i + j == this.cellValues.length - minus) {
+    //         // console.log(this.cellValues[j][i])
+    //         array.push(this.cellValues[j][i])
+    //       }
+    //     }
+    //   }
+    // }
+    // console.log(array)
+
+    // array = []
+    // for (let plus = 0; plus < this.cellValues.length - 1; plus++) {
+    //   for (let i = this.cellValues.length - 1; i > 0; i--) {
+    //     for (let j = this.cellValues.length - 1; j > 0; j--) {
+    //       if (i + j == this.cellValues.length + plus) {
+    //         // console.log(this.cellValues[j][i])
+    //         array.push(this.cellValues[j][i])
+    //       }
+    //     }
+    //   }
+    // }
+    // console.log(array)
+
+    // pionowo
+    for (let i = 0; i < this.columns.length; i++) {
+      array = []
+      for (let j = 0; j < this.rows.length; j++) {
+        // console.log(this.cellValues[j][i])
+        array.push({ value: this.cellValues[j][i], row: j, column: i })
+      }
+      // console.log(array);
+
+      for (let k = 0; k < array.length; k++) {
+        if (array[k].value != '' && (array[k].value == "X" || array[k].value == "O") && k + this.winningScore <= array.length) {
+          let isWinningLine = true;
+
+          for (let l = 0; l < this.winningScore; l++) {
+            if (array[k].value != array[k + l].value) {
+              isWinningLine = false;
+            }
+          }
+
+          if (isWinningLine) {
+            console.log("wygrana")
+            this.addPoint(array[k].value);
+
+            for (let l = 0; l < this.winningScore; l++) {
+              this.cellValues[array[k + l].row][array[k + l].column] = this.cellValues[array[k + l].row][array[k + l].column].toLowerCase();
+            }
+
+            k += 4;
+          }
+        }
+      }
+    }
+
+    // poziomo
+    for (let i = 0; i < this.rows.length; i++) {
+      array = []
+      for (let j = 0; j < this.columns.length; j++) {
+        // console.log(this.cellValues[i][j])
+        array.push({ value: this.cellValues[i][j], row: i, column: j })
+      }
+      // sprawdzanie wygranej
+      for (let k = 0; k < array.length; k++) {
+        if (array[k].value != '' && (array[k].value == "X" || array[k].value == "O") && k + this.winningScore <= array.length) {
+          let isWinningLine = true;
+
+          for (let l = 0; l < this.winningScore; l++) {
+            if (array[k].value != array[k + l].value) {
+              isWinningLine = false;
+            }
+          }
+
+          if (isWinningLine) {
+            console.log("wygrana")
+            this.addPoint(array[k].value);
+
+            for (let l = 0; l < this.winningScore; l++) {
+              this.cellValues[array[k + l].row][array[k + l].column] = this.cellValues[array[k + l].row][array[k + l].column].toLowerCase();
+            }
+
+            k += 4;
+          }
+        }
+      }
+    }
+  }
+
+  // ruch komputera (na razie losowo)
+  makeComputerMove() {
+    const emptyCells = [];
+    for (let i = 0; i < this.rows.length; i++) {
+      for (let j = 0; j < this.columns.length; j++) {
+        if (this.cellValues[i][j] === '') {
+          emptyCells.push({ row: i, column: j });
+        }
+      }
+    }
+    if (emptyCells.length > 0) {
+      const randomIndex = Math.floor(Math.random() * emptyCells.length);
+      const randomCell = emptyCells[randomIndex];
+      this.cellValues[randomCell.row][randomCell.column] = 'O';
+
+      if (!this.gameOver) {
+        this.switchPlayer();
+      }
+    }
+  }
+
+  // zmiana gracza po ruchu
+  switchPlayer() {
+    this.currentPlayer = this.currentPlayer == 'X' ? 'O' : 'X';
+  }
+
+  // remis
+  setDraw() {
+    this.gameOver = true;
+    this.winner = 'Draw';
+  }
+
+  // sprawdza czy sa wolne pola
+  isBoardFull(): boolean {
+    for (let i = 0; i < this.rows.length; i++) {
+      for (let j = 0; j < this.columns.length; j++) {
+        if (this.cellValues[i][j] == '') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // daje wartosc komorki
+  getCellValue(row: number, column: number): string {
+    return this.cellValues[row][column] || '';
+  }
+
+  // do kolorowania
+  isWinCell(rowIndex: number, columnIndex: number): boolean {
+    // console.table(this.cellValues);
+    if (this.cellValues[rowIndex][columnIndex] === 'x' || this.cellValues[rowIndex][columnIndex] === 'o') {
+      return true;
+    }
+    return false;
+  }
+
+  // dodaje punkty
+  addPoint(player: string) {
+    this.playerScores[player]++;
+    if (this.playerScores[player] === this.winningScore) {
+      this.gameOver = true;
+      this.winner = player;
+    }
   }
 }
